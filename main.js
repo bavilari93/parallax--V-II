d30f669f172e185cfb345ddcf2f0d4e0805847e0
Bianca A <frank@Bianca-Avila-4.local>
2018-08-12 16:20:46 +0900

    paralax speed fix

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 console.log('its connected')
-var parallax = document.getElementById("lax");
+$(() =>{
+const parallax = document.getElementById("parallax");
  console.log(parallax);
+
+
 // event listener 
 window.addEventListener('scroll', function(){
 // value that the user has scrolled from the top 
 	let offset = window.pageYOffset;
 	parallax.style.backgroundPositionY = offset * 0.7 + "px";
 })
+	
+})
+	
