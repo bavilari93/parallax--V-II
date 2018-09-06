ace0066d7cd1324237fd3a4f015a7ca1b9b39ef7
Bianca A <frank@Bianca-Avila-4.local>
2018-09-06 18:07:45 +0900

    parallax and section  fade in

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 $(() => {
-            const parallax = document.getElementById("parallax");
+
+		// top nav 
+		 const topNav = document.getElementById("myTopNav");
+
+    $(".icon").click(function() {
+        if (topNav.className === "top-nav") {
+            topNav.className += " responsive";
+            // else is in case of unclick
+        } else {
+            topNav.className = "top-nav"
+        }
+
+    })
+            
 
             // when page load 
             $(document).ready(function() {
                     var delay = 0;
+ 
 
                      $('.word:lt(3)').each(function() {
                                 $(this).delay(delay).animate({
                                     opacity: 0
-                                }, 500);
-                                delay += 500;
+                                }, 700);
+                                delay += 700;
                             })
                     setTimeout(function() {
                            $('.header').css('display', 'none');
-                           $('.main').css('display','flex');
-                        },3000);
+                           $('.main').css({'display':'flex', 'transition':'opacity 0.5s ease-in-out', 'left': '0'});
+                        },2000);
 
                     })
 
                 // event listener 
+
+                const parallax = document.getElementById("parallax");
                 window.addEventListener('scroll', function() {
 
                     const pageTop = $(this).scrollTop()
@@ -27,12 +43,13 @@ $(() => {
                     // value that the user has scrolled from the top 
                     let offset = window.pageYOffset;
                     parallax.style.backgroundPositionY = offset * 0.4 + "px";
+                    // transition of fad in fave out content
                     if (pageTop > 100) {
                         $('.tittle').fadeOut('slow');
-                        $('parallax').css("background-color", "maroon")
+                        $('#parallax').css("background-color", "maroon")
                     } else {
                         $('.tittle').fadeIn(3000);
-                        $('parallax').css("background-color", "black")
+                        $('#parallax').css("background-color", "black")
                     }
 
 
